e9f06077797a8ea0e3e4e03c0bec2f4fdba07a4f
ousttrue <ousttrue@gmail.com>
2019-09-09 01:39:15 +0900

    add DockSpace

--- src/extras.cpp
+++ src/extras.cpp
@@ -1,7 +1,31 @@
 #include "extras.h"
-#include <imgui.h>
-#include <imgui_internal.h>
-#include <imgui_impl_dx11.h>
-#include <imgui_impl_dx11.cpp>
-#include <imgui_impl_win32.h>
-#include <imgui_impl_win32.cpp>
+
+IMGUI_IMPL_API void exDockSpace(ImGuiID id, const ImVec2 size, ImGuiDockNodeFlags flags, const ImGuiWindowClass *window_class)
+{
+    ImGui::DockSpace(id, size, flags, window_class);
+}
+
+IMGUI_IMPL_API ImGuiID exDockSpaceOverViewport(ImGuiViewport *viewport, ImGuiDockNodeFlags flags, const ImGuiWindowClass *window_class)
+{
+    return ImGui::DockSpaceOverViewport(viewport, flags, window_class);
+}
+
+IMGUI_IMPL_API void exSetNextWindowDockID(ImGuiID dock_id, ImGuiCond cond)
+{
+    ImGui::SetNextWindowDockID(dock_id, cond);
+}
+
+IMGUI_IMPL_API void exSetNextWindowClass(const ImGuiWindowClass *window_class)
+{
+    ImGui::SetNextWindowClass(window_class);
+}
+
+IMGUI_IMPL_API ImGuiID exGetWindowDockID()
+{
+    return ImGui::GetWindowDockID();
+}
+
+IMGUI_IMPL_API bool exIsWindowDocked()
+{
+    return ImGui::IsWindowDocked();
+}
--- src/extras.h
+++ src/extras.h
@@ -1,2 +1,15 @@
 #pragma once
 #define IMGUI_IMPL_API extern "C" __declspec(dllexport)
+#include <imgui.h>
+#include <imgui_internal.h>
+#include <imgui_impl_dx11.h>
+#include <imgui_impl_dx11.cpp>
+#include <imgui_impl_win32.h>
+#include <imgui_impl_win32.cpp>
+
+IMGUI_IMPL_API void exDockSpace(ImGuiID id, const ImVec2 size, ImGuiDockNodeFlags flags, const ImGuiWindowClass *window_class);
+IMGUI_IMPL_API ImGuiID exDockSpaceOverViewport(ImGuiViewport *viewport, ImGuiDockNodeFlags flags, const ImGuiWindowClass *window_class);
+IMGUI_IMPL_API void exSetNextWindowDockID(ImGuiID dock_id, ImGuiCond cond); // set next window dock id (FIXME-DOCK)
+IMGUI_IMPL_API void exSetNextWindowClass(const ImGuiWindowClass *window_class); // set next window class (rare/advanced uses: provide hints to the platform back-end via altered viewport flags and parent/child info)
+IMGUI_IMPL_API ImGuiID exGetWindowDockID();
+IMGUI_IMPL_API bool exIsWindowDocked(); // is current window docked into another window?
